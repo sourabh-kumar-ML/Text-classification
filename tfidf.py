c2a7b4ce9807a4293341be54a480a9943dd6b81c
sourabh-kumar-ML <55852771+sourabh-kumar-ML@users.noreply.github.com>
2021-02-18 01:08:29 +0530

    Update tfidf.py

--- tfidf.py
+++ tfidf.py
@@ -7,6 +7,7 @@
 import pandas as pd
 import numpy as np
 from sklearn.preprocessing import OneHotEncoder
+from sklearn.mixture import GaussianMixture
 from sklearn.model_selection import train_test_split
 import tensorflow as tf
 import tensorflow.keras as keras
